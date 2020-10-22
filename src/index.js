9ee0cebbcde4c8f97fb6f89dc5cb9a1a1a711035
Suraj Karki <suraj.karki500@gmail.com>
2020-10-22 14:02:18 +0545

    Reconfigured the database connection procedure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
+// Local Module
+import logger from "./utils/logger";
+import app from "./server";
+
 // .env config
 dotenv.config();
 
-const client = MongoClient.connect(process.env.MONGO_URI, {
+MongoClient.connect(process.env.MONGO_URI, {
   wtimeout: 2500,
   w: "majority",
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then((client) => {
+    // inject db
+    logger.info("Database connected successfully.");
 
-export default client;
+    // Node Server
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      logger.info(`Listening on PORT ${port}.`);
+    });
+  })
+  .catch((err) => {
+    logger.error(`Error connecting to the MongoDB URI: ${err.stack}`);
+    process.exit(1);
+  });
--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ import express from "express";
 import morgan from "morgan";
 import compression from "compression";
 
-import client from "./index";
-import logger from "./utils/logger";
 import apiErrorHandler from "./error/api-error-handler";
 
 const app = express();
@@ -34,17 +32,4 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-const port = process.env.PORT || 8000;
-client
-  .then((client) => {
-    // inject db
-    logger.info("Database connected successfully.");
-  })
-  .catch((err) => {
-    logger.error(`Error connecting to the MongoDB URI: ${err.stack}`);
-    process.exit(1);
-  });
-
-app.listen(port, () => {
-  logger.info(`Listening on PORT ${port}.`);
-});
+export default app;
