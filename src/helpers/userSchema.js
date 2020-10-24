419a9872494cda705ee4367be7fcc6708c0bd79d
Suraj Karki <suraj.karki500@gmail.com>
2020-10-24 16:08:52 +0545

    Create userSchema

---
+++ src/helpers/userSchema.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+
+const schemas = {
+	userSIGNUP: Joi.object().keys({
+		username: Joi.string().min(4).max(20).required(),
+		email: Joi.string()
+			.email({
+				minDomainSegments: 2,
+				tlds: {
+					allow: ["com", "net"]
+				},
+			})
+			.required(),
+		firstName: Joi.string().min(2).max(20).required(),
+		lastName: Joi.string().min(2).max(20).required(),
+		role: Joi.string().valid("freelancer", "client", "admin").required(),
+		password: Joi.string()
+			.min(8)
+			.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+			.required(),
+	}),
+};
+export default schemas;
