d57c47fdbd772c62dfee8ff695b863c08c250d0e
Suraj Karki <suraj.karki500@gmail.com>
2020-10-27 15:49:37 +0545

    Created redis config.:

---
+++ src/helpers/init_redis.js
@@ -0,0 +1,30 @@
+import redis from "redis";
+
+import logger from "../utils/logger";
+
+const client = redis.createClient({
+  port: 6379,
+  host: "127.0.0.1",
+});
+
+client.on("connect", () => {
+  logger.info("Client connected to redis...");
+});
+
+client.on("ready", () => {
+  logger.info("Client connected to redis and ready to use...");
+});
+
+client.on("error", (err) => {
+  logger.error(err.message);
+});
+
+client.on("end", () => {
+  logger.info("Client disconnected from redis");
+});
+
+process.on("SIGINT", () => {
+  client.quit();
+});
+
+export default client;
