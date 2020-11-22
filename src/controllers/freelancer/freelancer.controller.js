78b80107e1afedbf4b3f4f193380ad52a7a0a0a7
Suraj Karki <suraj.karki500@gmail.com>
2020-11-22 16:23:57 +0545

    Removed extra user check from makeProfile handler.

--- src/controllers/freelancer/freelancer.controller.js
+++ src/controllers/freelancer/freelancer.controller.js
@@ -8,67 +8,33 @@ export async function makeProfile(req, res, next) {
   try {
     const profileInfo = req.body;
     const { aud } = req.jwt;
-
-    const user = await DAOs.usersDAO.getUserById(aud);
-    if (user.success) {
-      const info = {
-        user: ObjectId(aud),
-        ...profileInfo,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-      const result = await DAOs.freelancersDAO.createProfile(info);
-      if (result.success) {
-        const data = { message: "Your profile created successfully." };
-        return writeServerResponse(
-          res,
-          data,
-          result.statusCode,
-          "application/json"
-        );
-      }
-      return writeServerResponse(
-        res,
-        result.data.error,
-        result.statusCode,
-        "application/json"
-      );
-    } else {
-      next(ApiError.badRequest("No user exist with that user id."));
-      return;
-    }
-  } catch (error) {
-    next(ApiError.internal(`Something went wrong: ${error.message}`));
-    return;
-  }
-}
-
-export async function me(req, res, next) {
-  try {
-    const { aud } = req.jwt;
-    const result = await DAOs.freelancersDAO.me(aud);
+    const info = {
+      user: ObjectId(aud),
+      ...profileInfo,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const result = await DAOs.freelancersDAO.createProfile(info);
     if (result.success) {
-      const data = { status: "success", data: result.data };
-      return writeServerResponse(
-        res,
-        data,
-        result.statusCode,
-        "application/json"
-      );
-    }
-    const user = await DAOs.usersDAO.getUserById(aud);
-    if (user.success) {
       const data = {
         status: "success",
-        data: { ...user.data, password: null },
+        data: {
+          message: "Your profile created successfully.",
+        },
       };
       return writeServerResponse(
         res,
         data,
-        user.statusCode,
+        result.statusCode,
         "application/json"
       );
     }
+    return writeServerResponse(
+      res,
+      { status: "failed", data: result.data },
+      result.statusCode,
+      "application/json"
+    );
   } catch (error) {
     next(ApiError.internal(`Something went wrong: ${error.message}`));
     return;
