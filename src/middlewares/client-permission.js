bc937e9c39bfff6238933e38ba31d943f672b687
surajkarki66 <suraj.karki500@protonmail.com>
2021-02-26 14:35:09 +0545

    Create client-permission function.

---
+++ src/middlewares/client-permission.js
@@ -0,0 +1,10 @@
+import ApiError from "../error/ApiError";
+
+export const onlyClientCanDoThisAction = (req, res, next) => {
+  const { role } = req.jwt;
+  if (role === "client") {
+    return next();
+  }
+  next(ApiError.forbidden("Access denied: Only client can do this action"));
+  return;
+};
