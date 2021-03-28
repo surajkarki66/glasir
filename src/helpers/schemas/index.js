37330a22e76471259e8300719e33cb555ec6187a
surajkarki66 <suraj.karki500@protonmail.com>
2021-03-28 11:54:42 +0545

    Finished createJob method.

--- src/helpers/schemas/index.js
+++ src/helpers/schemas/index.js
@@ -2,5 +2,12 @@ import userSchema from "./userSchema";
 import freelancerSchema from "./freelancerSchema";
 import clientSchema from "./clientSchema";
 import commonSchema from "./commonSchema";
+import jobSchema from "./jobSchema";
 
-export default { userSchema, freelancerSchema, clientSchema, commonSchema };
+export default {
+  userSchema,
+  freelancerSchema,
+  clientSchema,
+  commonSchema,
+  jobSchema,
+};
