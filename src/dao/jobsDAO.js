4f6e52155e003bad39e16d1ecac2aab5e3338012
surajkarki66 <suraj.karki500@protonmail.com>
2021-06-26 23:31:40 +0545

    Create getJobs dao method

--- src/dao/jobsDAO.js
+++ src/dao/jobsDAO.js
@@ -3,6 +3,8 @@ import logger from "../configs/logger";
 
 class JobsDAO {
   static #jobs;
+  static #DEFAULT_PROJECT = {};
+  static #DEFAULT_SORT = { createdAt: -1 };
   static async injectDB(conn) {
     if (JobsDAO.#jobs) {
       return;
@@ -43,6 +45,52 @@ class JobsDAO {
     } catch (error) {
       logger.error(`Error occurred while adding new job, ${error.message}.`);
       throw error;
+    }
+  }
+  static async getJobs({ filters = null, page = 0, jobsPerPage = 20 } = {}) {
+    let queryParams = {};
+
+    if (filters) {
+      /** @TODO
+       * Add here filter objects
+       */
+    }
+    const {
+      query = {},
+      project = JobsDAO.#DEFAULT_PROJECT,
+      sort = JobsDAO.#DEFAULT_SORT,
+    } = queryParams;
+    let cursor;
+    try {
+      cursor = await JobsDAO.#jobs.find(query).project(project).sort(sort);
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return {
+        success: false,
+        data: [],
+        totalNumJobs: 0,
+        statusCode: 404,
+      };
+    }
+    const displayCursor = cursor
+      .skip(parseInt(page) * parseInt(jobsPerPage))
+      .limit(parseInt(jobsPerPage));
+
+    try {
+      const documents = await displayCursor.toArray();
+      const totalNumJobs =
+        parseInt(page) === 0 ? await JobsDAO.#jobs.countDocuments({}) : 0;
+      return {
+        success: true,
+        data: documents,
+        totalNumJobs,
+        statusCode: documents.length > 0 ? 200 : 404,
+      };
+    } catch (e) {
+      logger.error(
+        `Unable to convert cursor to array or problem counting documents, ${e.message}`,
+      );
+      throw e;
     }
   }
 }
