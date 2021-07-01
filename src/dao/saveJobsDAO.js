27290d0117a46622accbd3b7df9c542512f09560
surajkarki66 <suraj.karki500@protonmail.com>
2021-07-01 17:41:01 +0545

    Create createSaveJob dao's handler

--- src/dao/saveJobsDAO.js
+++ src/dao/saveJobsDAO.js
@@ -24,6 +24,29 @@ class SaveJobsDAO {
       throw e;
     }
   }
+  static async createSaveJob(saveJobInfo) {
+    try {
+      const result = await SaveJobsDAO.#saveJobs.insertOne(saveJobInfo);
+      if (result.insertedCount === 1) {
+        const data = result.ops[0];
+        return {
+          success: true,
+          data: data,
+          statusCode: 201,
+        };
+      }
+    } catch (e) {
+      if (String(e).startsWith("MongoError: Document failed validation")) {
+        return {
+          success: false,
+          data: { error: "Document failed validation" },
+          statusCode: 422,
+        };
+      }
+      logger.error(`Error occurred while adding new job, ${e}.`);
+      throw e;
+    }
+  }
 }
 
 export default SaveJobsDAO;
